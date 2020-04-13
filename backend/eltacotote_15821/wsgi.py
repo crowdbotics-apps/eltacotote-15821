e21a07fa3546658a9c3cb84436851f36bfccc406
Crowdbotics <team@crowdbotics.com>
2020-04-13 21:48:33 +0000

    Initial commit.

---
+++ backend/eltacotote_15821/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for eltacotote_15821 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'eltacotote_15821.settings')
+
+application = get_wsgi_application()
